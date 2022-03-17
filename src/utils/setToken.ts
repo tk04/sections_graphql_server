aeffef12e6e165004dc72a6dece213ddb0fd34eb
taloufi <taloufi@iu.edu>
2022-03-17 09:21:48 -0400

    removed domain from cookie

--- src/utils/setToken.ts
+++ src/utils/setToken.ts
@@ -8,6 +8,6 @@ export const setToken = (userId: string, res: Response) => {
     maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
     secure: process.env.NODE_ENV === "production",
     httpOnly: true,
-    domain: process.env.NODE_ENV === "production" ? "vercel.app" : undefined,
+    // domain: process.env.NODE_ENV === "production" ? "vercel.app" : undefined,
   });
 };
