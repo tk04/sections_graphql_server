e48580f788bd55e7b30bb4b7b51fe4e02e6f3e01
taloufi <taloufi@iu.edu>
2022-03-17 09:18:05 -0400

    changed domain on cookie

--- dist/utils/setToken.js
+++ dist/utils/setToken.js
@@ -13,9 +13,7 @@ const setToken = (userId, res) => {
         maxAge: 1000 * 60 * 60 * 24 * 30,
         secure: process.env.NODE_ENV === "production",
         httpOnly: true,
-        domain: process.env.NODE_ENV === "production"
-            ? ".vercel.app"
-            : undefined,
+        domain: process.env.NODE_ENV === "production" ? "vercel.app" : undefined,
     });
 };
 exports.setToken = setToken;
